bf762bff66e28e293937e0df40986b89dd318939
Ken4yr <pyg999888@gmail.com>
2024-01-06 14:17:46 +0300

    попытка игнора бин и обдж

--- Program.cs
+++ Program.cs
@@ -12,41 +12,38 @@ void generateArray(string[] array) //Функция ввода массива
     Console.WriteLine(); // Перевод строки (чтобы отделять массивы)
 }
 
-
-string[] thirdsimvol(string[] stringArray) //Основная функция
+string[] thirdsimvol(string[] array) //Основная функция
 {
     int kol = 0; //Начало счетчика
-    for (int i = 0; i < stringArray.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if (stringArray[i].Length <= 3)
+        if (array[i].Length <= 3)
         kol++; //Подсчет кол-ва элементов с тремя и менее символами
     }
 
     
     string[] rezult = new string[kol]; //Создание нового массива размерностью с kol
     int j = 0;
-    for (int i = 0; i < stringArray.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if (stringArray[i].Length <= 3)
+        if (array[i].Length <= 3)
         {
-            rezult[j] = stringArray[i]; //Внос элементов с тремя и менее символами с введенного массива в новый
+            rezult[j] = array[i]; //Внос элементов с тремя и менее символами с введенного массива в новый
             j++;
         }
     }
     return rezult;
 }
 
-
-void printArray(string[] stringArray) //Функция вывода массива
+void printArray(string[] array) //Функция вывода массива
 {
     Console.Write("[");
-    for (int i = 0; i < stringArray.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"‘{ stringArray[i]}’, ");
+        Console.Write($"‘{ array[i]}’, ");
     }
     Console.Write("]");
 }
-
 
 generateArray(Array);
 printArray(thirdsimvol(Array));
